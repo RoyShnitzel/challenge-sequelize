a2bd19133f7bb8375be2a5334c90527f32050d02
OfirSimhi1612 <67858057+OfirSimhi1612@users.noreply.github.com>
2020-10-02 15:05:44 +0300

    basic test for findAll and update working

--- mySequelize.js
+++ mySequelize.js
@@ -1,13 +1,47 @@
+const { SET } = require('./options/SET');
+const { WHERE } = require('./options/WHERE');
+
 class MySequelize {
   constructor(connect, tableName) {
     this.connection = connect;
     this.table = tableName;
   }
 
+
   async findAll() {
-    console.log("started");
     const results = await this.connection.query(`SELECT * FROM ${this.table}`)
-    return results
+    return results[0]
+  }
+
+
+
+  async update(newDetsils, { where, ...restOptions }) {
+    if (!newDetsils) {
+      throw 'Must sumbit colums and values to update'
+    }
+    if (!where) {
+      throw `Must sumbit at list one 'where' conditon in the options object`
+    }
+
+    const SET_Statment = SET(newDetsils)
+    const WHERE_Statment = WHERE(where)
+
+    const restStatments = '';
+
+    if (restOptions.order) {
+      //oder option
+    }
+    if (restOptions.limit) {
+      //limit optins
+    }
+
+    // console.log(SET_Statment, WHERE_Statment)
+
+    const newObjects = await this.connection.query(`UPDATE ${this.table} SET ${SET_Statment} WHERE ${WHERE_Statment}`)
+
+    // console.log(newObjects)
+
+    return newObjects
   }
 }
 
--- test.test.js
+++ test.test.js
@@ -10,7 +10,7 @@ describe("first test", () => {
       host: "localhost",
       user: "root",
       password: "O16s12@96",
-      database: "music_app_dev",
+      database: "challenge_sequelize",
       multipleStatements: true,
     });
   });
@@ -19,18 +19,104 @@ describe("first test", () => {
     await connection.end();
   });
 
-  describe('test', () => {
 
-    test("findAll test", async () => {
-      const Song = new MySequelize(mysqlCon, "songs");
-      const myResults = await Song.findAll();
 
-      const results = await mysqlCon.query(`SELECT * FROM songs`)
+  describe('findAll() test', () => {
 
-      expect(myResults.length).toBe(results.length)
+    beforeAll(async () => {
+      await mysqlCon.query('TRUNCATE TABLE `users`')
+
+      await mysqlCon.query(`INSERT INTO users (name, email, password, is_admin)
+          VALUES ('Dani', 'dani@gmail.com', '123456789', false),
+          ('Yoni', 'yoni@gmail.com', '987654321', false),
+          ('Ron', 'ron@gmail.com', '192837465', false),
+          ('Dana', 'dana@gmail.com', '918273645', True),
+          ('Yuval', 'yuval@gmail.com', '65748493021', false);`)
     });
+
+    afterAll(async () => {
+      await mysqlCon.query('TRUNCATE TABLE `users`')
+    })
+
+    test("no conditions test", async () => {
+      const User = new MySequelize(mysqlCon, "users");
+      const myResults = await User.findAll();
+
+      const results = await mysqlCon.query(`SELECT * FROM users`)
+
+      expect(myResults.length).toBe(results[0].length)
+
+      expect(myResults[0].name).toBe('Dani');
+      expect(myResults[4].name).toBe('Yuval');
+
+      expect(myResults[1].password).toBe('987654321');
+      expect(myResults[3].password).toBe('918273645');
+
+    });
+
+    // test("only WHERE test", async () => {
+
+    //   const User = new MySequelize(mysqlCon, "users");
+    //   const myResults = await User.findAll({
+    //     where: {
+    //       name: 'Yoni'
+    //     }
+    //   });
+
+    //   const results = await mysqlCon.query(`SELECT * FROM songs`)
+
+    //   expect(myResults.length).toBe(results.length)
+    // });
+
+
   })
 
-});
+
+  describe('update() test', () => {
+
+    beforeAll(async () => {
+      await mysqlCon.query('TRUNCATE TABLE `users`')
+
+      await mysqlCon.query(`INSERT INTO users (name, email, password, is_admin)
+          VALUES ('Dani', 'dani@gmail.com', '123456789', false),
+          ('Yoni', 'yoni@gmail.com', '987654321', false),
+          ('Ron', 'ron@gmail.com', '192837465', false),
+          ('Dana', 'dana@gmail.com', '918273645', True),
+          ('Yuval', 'yuval@gmail.com', '65748493021', false);`)
+    });
+
+    afterAll(async () => {
+      await mysqlCon.query('TRUNCATE TABLE `users`')
+    })
+
+
+
+    test('update test', async () => {
+
+      const allUsers = await mysqlCon.query(`SELECT * FROM users`)
+
+      expect(allUsers[0][0].name).toBe('Dani')
+      expect(allUsers[0][0].email).toBe('dani@gmail.com')
+
+      const User = new MySequelize(mysqlCon, 'users');
+      await User.update({ name: 'Yoav', email: 'yoav@gmail.com' }, {
+        where: {
+          id: allUsers[0][0].id
+        }
+      })
+
+      const user = await mysqlCon.query(`SELECT * FROM users WHERE id = ${allUsers[0][0].id}`)
+
+      expect(user[0][0].name).toBe('Yoav')
+      expect(user[0][0].email).toBe('yoav@gmail.com')
+
+    })
+  })
+
+
+})
+
+
+
 
 
