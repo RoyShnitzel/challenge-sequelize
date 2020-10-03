85b91ac0de5e4221358a5e742df6d5cb823cef03
MrMoshkovitz <gal@gmail.com>
2020-10-03 18:46:22 +0300

    hard delete updated

--- mySequelize.js
+++ mySequelize.js
@@ -1,156 +1,164 @@
 const mysql = require("mysql2");
-const softDelete = require('./sqlQueries/softDelete');
+const softDelete = require("./sqlQueries/softDelete");
 const getDate = require("./helpers/getDate");
-const config = require('./options/optins')
-const { SET } = require('./options/SET')
-
+const config = require("./options/optins");
+const { SET } = require("./options/SET");
+const hardDelete = require("./sqlQueries/hardDelete");
 
 class MySequelize {
-  constructor(connect, tableName) {
-    this.connection = connect;
-    this.table = tableName;
-  }
+	constructor(connect, tableName) {
+		this.connection = connect;
+		this.table = tableName;
+	}
 
-  async delete(id) {
-    console.log("Delete Function Started");
-    const date = getDate()
-    console.log(`\"${date}\"`)
-    const query = softDelete(`\"${date}\"`, id)
-    const results = await this.connection.query(query);
-    // console.log(`${id} Has Been Soft Deleted`)
-    return results
-  }
+	async delete(options) {
+		if (options) {
+      let id = options.where.id
+			const conditions = Object.keys(options);
+			if (conditions.includes("force") && options["force"] === true) {
+				const query = hardDelete(id);
+				const results = await this.connection.query(query);
+				return results;
+			} else {
+				const date = getDate();
+				console.log(`\"${date}\"`);
+				const query = softDelete(`\"${date}\"`, id);
+				const results = await this.connection.query(query);
+				return results;
+			}
+		}
+	}
 
-  async restore(id) {
-    console.log("Restore Function Started");
-    const query = softDelete(null, id)
-    const results = await this.connection.query(query);
-    console.log(`User ID: ${id} Has Been Resotred`)
-    return results
-  }
+	async restore(options) {
+  
+    let id = options ? options.where.id : null
+		const query = softDelete(null, id);
+		const results = await this.connection.query(query);
+		console.log(`User ID: ${id} Has Been Resotred`);
+		return results;
+	}
 
-  async findAll(options) {
-    let optionsStatment = {}
+	async findAll(options) {
+		let optionsStatment = {};
 
-    if (options) {
-      const conditions = Object.keys(options)
-      optionsStatment = conditions.reduce((statment, condition) => {
-        if (condition === 'include') {
-          return statment
-        }
-        statment[condition] = config[condition](options[condition])
-        return statment
-      }, {})
+		if (options) {
+			const conditions = Object.keys(options);
+			optionsStatment = conditions.reduce((statment, condition) => {
+				if (condition === "include") {
+					return statment;
+				}
+				statment[condition] = config[condition](options[condition]);
+				return statment;
+			}, {});
+		}
 
-    }
-
-    let results = await this.connection.query(`
-    SELECT ${optionsStatment.attributes ? optionsStatment.attributes : '*'} 
+		let results = await this.connection.query(`
+    SELECT ${optionsStatment.attributes ? optionsStatment.attributes : "*"} 
     FROM ${this.table} 
-    ${optionsStatment.where ? optionsStatment.where : ''} 
-    ${optionsStatment.order ? optionsStatment.order : ''} 
-    ${optionsStatment.limit ? optionsStatment.limit : ''}`)
+    ${optionsStatment.where ? optionsStatment.where : ""} 
+    ${optionsStatment.order ? optionsStatment.order : ""} 
+    ${optionsStatment.limit ? optionsStatment.limit : ""}`);
 
-    results = results[0]
+		results = results[0];
 
-    if (options && options.include) {
-      return await config.include(results, options.include, this.connection)
-    } else {
-      return results
-    }
-  }
+		if (options && options.include) {
+			return await config.include(results, options.include, this.connection);
+		} else {
+			return results;
+		}
+	}
 
-  async update(newDetsils, options) {
+	async update(newDetsils, options) {
+		if (!newDetsils) {
+			throw "Must sumbit colums and values to update";
+		}
+		if (!options.where) {
+			throw `Must sumbit at list one 'where' conditon in the options object`;
+		}
 
-    if (!newDetsils) {
-      throw 'Must sumbit colums and values to update'
-    }
-    if (!options.where) {
-      throw `Must sumbit at list one 'where' conditon in the options object`
-    }
+		const conditions = Object.keys(options);
+		const optionsStatment = conditions.reduce((statment, condition) => {
+			statment[condition] = config[condition](options[condition]);
+			return statment;
+		}, {});
 
-    const conditions = Object.keys(options)
-    const optionsStatment = conditions.reduce((statment, condition) => {
-      statment[condition] = config[condition](options[condition])
-      return statment
-    }, {})
+		const SET_Statment = SET(newDetsils);
 
-    const SET_Statment = SET(newDetsils)
-
-    const newObjects = await this.connection.query(`UPDATE ${this.table} 
+		const newObjects = await this.connection.query(`UPDATE ${this.table} 
     ${SET_Statment}
     ${optionsStatment.where}
-    ${optionsStatment.order ? optionsStatment.order : ''}
-    ${optionsStatment.limit ? optionsStatment.limit : ''}`)
+    ${optionsStatment.order ? optionsStatment.order : ""}
+    ${optionsStatment.limit ? optionsStatment.limit : ""}`);
 
-    return newObjects
-  }
+		return newObjects;
+	}
 
-  async create(obj) {
-    console.log("started create");
-    let columns = [];
-    let values = [];
-    for (const [key, value] of Object.entries(obj)) {
-      columns.push(key);
-      if (typeof (value) === 'boolean') {
-        values.push(value)
-      } else {
-        values.push(`\'${value}\'`);
-      }
+	async create(obj) {
+		console.log("started create");
+		let columns = [];
+		let values = [];
+		for (const [key, value] of Object.entries(obj)) {
+			columns.push(key);
+			if (typeof value === "boolean") {
+				values.push(value);
+			} else {
+				values.push(`\'${value}\'`);
+			}
+		}
+		columns = `(${columns.toString()})`;
+		values = `(${values.toString()})`;
+		const result = await this.connection.query(
+			`INSERT INTO ${this.table} ${columns} VALUES ${values}`
+		);
+	}
 
-    }
-    columns = `(${columns.toString()})`;
-    values = `(${values.toString()})`;
-    const result = await this.connection.query(
-      `INSERT INTO ${this.table} ${columns} VALUES ${values}`
-    );
-  }
+	async bulkCreate(arr) {
+		console.log("started create");
+		let columns = Object.keys(arr[0]);
+		let values = arr.map((obj) => {
+			let arrOfValues = Object.values(obj);
+			return `(${arrOfValues
+				.map((value) => (typeof value === "boolean" ? value : `\'${value}\'`))
+				.toString()})`;
+		});
 
-  async bulkCreate(arr) {
-    console.log("started create");
-    let columns = Object.keys(arr[0]);
-    let values = arr.map((obj) => {
-      let arrOfValues = Object.values(obj);
-      return `(${arrOfValues.map((value) => typeof (value) === 'boolean' ? value : `\'${value}\'`).toString()})`;
-    });
+		columns = `(${columns.toString()})`;
+		values = `${values.toString()}`;
+		console.log(columns);
+		console.log(values);
+		const result = await this.connection.query(
+			`INSERT INTO ${this.table} ${columns} VALUES ${values}`
+		);
+	}
 
-    columns = `(${columns.toString()})`;
-    values = `${values.toString()}`;
-    console.log(columns);
-    console.log(values);
-    const result = await this.connection.query(
-      `INSERT INTO ${this.table} ${columns} VALUES ${values}`
-    );
-  }
+	async findByPk(id) {
+		const results = await this.connection.query(
+			`SELECT * FROM ${this.table} WHERE id = ${id}`
+		);
+		return results[0];
+	}
 
-  async findByPk(id) {
-    const results = await this.connection.query(`SELECT * FROM ${this.table} WHERE id = ${id}`);
-    return results[0]
-  }
+	async findOne(options) {
+		let optionsStatment = {};
 
-  async findOne(options) {
-    let optionsStatment = {};
+		if (options) {
+			const conditions = Object.keys(options);
+			optionsStatment = conditions.reduce((statment, condition) => {
+				statment[condition] = config[condition](options[condition]);
+				return statment;
+			}, {});
+		}
 
-    if (options) {
-      const conditions = Object.keys(options)
-      optionsStatment = conditions.reduce((statment, condition) => {
-        statment[condition] = config[condition](options[condition])
-        return statment
-      }, {})
-
-    }
-
-    const results = await this.connection.query(`SELECT 
-    ${optionsStatment.attributes ? optionsStatment.attributes : '*'} 
+		const results = await this.connection.query(`SELECT 
+    ${optionsStatment.attributes ? optionsStatment.attributes : "*"} 
     FROM ${this.table}
-    ${optionsStatment.include ? optionsStatment.include : ''}
-    ${optionsStatment.where ? optionsStatment.where : ''} 
-    ${optionsStatment.order ? optionsStatment.order : ''} 
-    ${optionsStatment.limit ? optionsStatment.limit : ''}
-    LIMIT 1`)
-    return results[0]
-  }
+    ${optionsStatment.include ? optionsStatment.include : ""}
+    ${optionsStatment.where ? optionsStatment.where : ""} 
+    ${optionsStatment.order ? optionsStatment.order : ""} 
+    ${optionsStatment.limit ? optionsStatment.limit : ""}
+    LIMIT 1`);
+		return results[0];
+	}
 }
-
 
 module.exports = { MySequelize };
