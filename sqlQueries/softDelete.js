93176174fe6cf058ec5e9c453d5efd2d334fe4b4
OfirSimhi1612 <67858057+OfirSimhi1612@users.noreply.github.com>
2020-10-03 21:12:37 +0300

    removed sqlScripts folder

--- sqlQueries/softDelete.js
+++
@@ -1,6 +0,0 @@
-// UPDATE users SET deleted_at=CURRENT_TIME() WHERE deleted_at IS NULL AND id = ?
-// UPDATE users SET deleted_at=? WHERE ${id ? `id =${id}` : `deleted_at IS Not Null`}`
-
-module.exports = (deleted_at, id) => {
-    return `UPDATE users SET deleted_at=${deleted_at} WHERE ${id ? `id =${id}` : `deleted_at IS Not Null`}`
-}
