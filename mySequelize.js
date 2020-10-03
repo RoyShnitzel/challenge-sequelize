93a143b9d7b994a059dbdf6544fe89861b4a2096
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-10-03 12:03:19 +0300

    base of create and bulkcreate+tests

--- mySequelize.js
+++ mySequelize.js
@@ -21,8 +21,40 @@ class MySequelize {
 
   async findAll() {
     console.log("started");
-    const results = await this.connection.query(`SELECT * FROM ${this.table}`)
-    return results
+    const results = await this.connection.query(`SELECT * FROM ${this.table}`);
+    return results;
+  }
+
+  async create(obj) {
+    console.log("started create");
+    let columns = [];
+    let values = [];
+    for (const [key, value] of Object.entries(obj)) {
+      columns.push(key);
+      values.push(`\'${value}\'`);
+    }
+    columns = `(${columns.toString()})`;
+    values = `(${values.toString()})`;
+    const result = await this.connection.query(
+      `INSERT INTO ${this.table} ${columns} VALUES ${values}`
+    );
+  }
+
+  async bulkCreate(arr) {
+    console.log("started create");
+    let columns = Object.keys(arr[0]);
+    let values = arr.map((obj) => {
+      let arrOfValues = Object.values(obj);
+      return `(${arrOfValues.map((value) => `\'${value}\'`).toString()})`;
+    });
+
+    columns = `(${columns.toString()})`;
+    values = `${values.toString()}`;
+    console.log(columns);
+    console.log(values);
+    const result = await this.connection.query(
+      `INSERT INTO ${this.table} ${columns} VALUES ${values}`
+    );
   }
 }
 // setTimeout(async () => {
@@ -31,5 +63,3 @@ class MySequelize {
 // }, 1000);
 
 module.exports = { MySequelize };
-
-
--- test.test.js
+++ test.test.js
@@ -4,13 +4,12 @@ const mysql = require("mysql2/promise");
 let mysqlCon;
 
 describe("first test", () => {
-
   beforeAll(async () => {
     mysqlCon = await mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "O16s12@96",
-      database: "music_app_dev",
+      password: "yalla007",
+      database: "music-app",
       multipleStatements: true,
     });
   });
@@ -19,18 +18,61 @@ describe("first test", () => {
     await connection.end();
   });
 
-  describe('test', () => {
-
+  describe("test", () => {
     test("findAll test", async () => {
       const Song = new MySequelize(mysqlCon, "songs");
       const myResults = await Song.findAll();
 
-      const results = await mysqlCon.query(`SELECT * FROM songs`)
+      const results = await mysqlCon.query(`SELECT * FROM songs`);
 
-      expect(myResults.length).toBe(results.length)
+      expect(myResults.length).toBe(results.length);
     });
-  })
 
+    test("create test", async () => {
+      const Album = new MySequelize(mysqlCon, "albums");
+      await Album.create({
+        id: 21,
+        name: "test",
+        cover_img: "jdhhf.jpeg",
+        created_at: "2020-10-10",
+        upload_at: "2020-10-10",
+      });
+
+      const results = await mysqlCon.query(
+        `SELECT * FROM albums WHERE id = 21`
+      );
+
+      expect(results[0][0].name).toBe("test");
+    });
+
+    test("bulkCreate test", async () => {
+      const Album = new MySequelize(mysqlCon, "albums");
+      await Album.bulkCreate([
+        {
+          id: 22,
+          name: "test2",
+          cover_img: "jdhhf2.jpeg",
+          created_at: "2020-10-10",
+          upload_at: "2020-10-10",
+        },
+        {
+          id: 23,
+          name: "test3",
+          cover_img: "jdhhf3.jpeg",
+          created_at: "2020-10-10",
+          upload_at: "2020-10-10",
+        },
+      ]);
+
+      const results = await mysqlCon.query(
+        `SELECT * FROM albums WHERE id = 22`
+      );
+      const results2 = await mysqlCon.query(
+        `SELECT * FROM albums WHERE id = 23`
+      );
+
+      expect(results[0][0].name).toBe("test2");
+      expect(results2[0][0].name).toBe("test3");
+    });
+  });
 });
-
-
