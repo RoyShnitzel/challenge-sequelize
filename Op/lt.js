4b7749d4c8f8d82038937a626601343998ddd8a9
RoyShnitzel <67857836+RoyShnitzel@users.noreply.github.com>
2020-10-04 14:31:45 +0300

    fixes

--- Op/lt.js
+++ Op/lt.js
@@ -1,16 +1,14 @@
 function lt(object) {
+  const GTstatment = Object.keys(object).reduce((statment, key, index) => {
+    if (index === Object.keys(object).length - 1) {
+      return (statment += `${key} < ${object[key]}`);
+    } else {
+      return (statment += `${key} < ${object[key]} AND `);
+    }
+  }, "");
 
-    const GTstatment = Object.keys(object).reduce((statment, key, index) => {
-
-        if (index === Object.keys(object).length - 1) {
-            return statment += `${key} < ${object[key]}`
-        } else {
-            return statment += `${key} < ${object[key]} AND `
-        }
-    }, '')
-
-    console.log(GTstatment)
-    return GTstatment
+  console.log(GTstatment);
+  return GTstatment;
 }
 
-module.exports = lt
+module.exports = { lt };
